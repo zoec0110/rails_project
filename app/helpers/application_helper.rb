0a57fe28d0fd978c28dd2c27288c7e414442ef31
Yin <zoe@statementdog.com>
2022-10-25 16:56:57 +0800

    add current user in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:current_user_id])
+  end
+
+  def user_signed_in?
+    @current_user.present?
+  end
 end
