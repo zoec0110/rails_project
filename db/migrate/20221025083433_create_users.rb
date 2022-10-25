7d2dc0d115285596e2ec89d4132cb4d6e60fdd07
Yin <zoe@statementdog.com>
2022-10-25 16:43:17 +0800

    create user model

--- db/migrate/20221025083433_create_users.rb
+++ db/migrate/20221025083433_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string 'email', default: '', null: false
-      t.string 'password'
+      t.string "email", default: "", null: false
+      t.string "password"
       t.timestamps
     end
   end
