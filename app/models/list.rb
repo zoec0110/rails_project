aa3d47e3df3006e250822a69cdd397d50f3b401e
Yin <zoe@statementdog.com>
2022-10-27 17:56:49 +0800

    consider different user to revise list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,21 +1,12 @@
 class List < ApplicationRecord
   validates_presence_of :name
+  before_create :add_order
   belongs_to :user
   has_many :list_stockships
   has_many :stocks, through: :list_stockships
 
-  def add_order
-    if List.count.zero?
-      list_qty = List.count
-      self.number = list_qty + 1
-    else
-      list_order = List.order(:number).last
-      self.number = list_order.number + 1
-    end
-  end
-
   def move_up_number
-    last_list = List.where('number < ?', number).order('number asc')
+    last_list = user.lists.where('number < ?', number).order('number desc')
     current_number = number
     self.number = last_list.first.number
     last_list.first.number = current_number
@@ -24,12 +15,23 @@ class List < ApplicationRecord
   end
 
   def move_down_number
-    List.where('number > ?', number).order('number asc')
-    next_list = List.where('number > ?', number).order('number asc')
+    next_list = user.lists.where('number > ?', number).order('number asc')
     current_number = number
     self.number = next_list.first.number
     next_list.first.number = current_number
     save
     next_list.first.save
   end
+
+  private
+
+    def add_order
+      if user.lists.count.zero?
+        list_qty = 0
+        self.number = list_qty + 1
+      else
+        list_order = user.lists.maximum(:number)
+        self.number = list_order + 1
+      end
+    end
 end
